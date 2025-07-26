c54faaabc02037cbbdfa43e76fb9762d7b2be041
Bismarck <emiliocardero1@gmail.com>
2025-07-26 10:34:27 -0500

    small performance optimizations in some helper functions

--- helpers/convert_8_bytes_into_int64.go
+++ helpers/convert_8_bytes_into_int64.go
@@ -3,7 +3,8 @@ package helpers
 import "encoding/binary"
 
 func Convert8BytesIntoSingleInt64(byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7 byte) int64 {
-	return int64(binary.BigEndian.Uint64([]byte{byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7}))
+	array := [8]byte{byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7}
+	return int64(binary.BigEndian.Uint64(array[:]))
 }
 
 func ConvertBytesArrayIntoSingleInt64(byteArray []byte) int64 {
@@ -11,13 +12,5 @@ func ConvertBytesArrayIntoSingleInt64(byteArray []byte) int64 {
 		return 0
 	}
 
-	return Convert8BytesIntoSingleInt64(
-		byteArray[0],
-		byteArray[1],
-		byteArray[2],
-		byteArray[3],
-		byteArray[4],
-		byteArray[5],
-		byteArray[6],
-		byteArray[7])
+	return int64(binary.BigEndian.Uint64(byteArray[:8]))
 }
--- helpers/validate_client_version.go
+++ helpers/validate_client_version.go
@@ -3,5 +3,6 @@ package helpers
 import "expansion-gateway/enums"
 
 func ValidateClientVersion(clientType enums.ClientType, version byte) bool {
+	// TODO: Implement actual version validation logic per client type.
 	return true
 }
