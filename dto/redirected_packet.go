f5e944544fea4d2ac1bb27a8a0bb65fbe40272db
Bismarck <emiliocardero1@gmail.com>
2025-11-27 13:24:54 -0600

    redirected packet, small fixes

--- dto/redirected_packet.go
+++ dto/redirected_packet.go
@@ -8,9 +8,10 @@ import (
 )
 
 type RedirectedPacket struct {
-	OwnerID        int64    // the id of the session of the user that owns this packet
-	Signature      [64]byte // the signature of the data the user is going to send
-	SubscriptionID int64    // the id of the subscription this packet is validating
+	OwnerID                  int64    // the id of the session of the user that owns this packet
+	Signature                [64]byte // the signature of the data the user is going to send
+	SubscriptionID           int64    // the id of the subscription this packet is validating
+	ClientPublicEphemeralKey []byte   // the public ephemeral key of the client, used to generate the final key
 }
 
 func (packet *RedirectedPacket) GetPacketType() enums.PacketType {
@@ -26,25 +27,57 @@ func (packet *RedirectedPacket) GetPayload() string {
 }
 
 func (packet *RedirectedPacket) Marshal() ([]byte, errors.GatewayError) {
-	answer := make([]byte, 0, 1+8+64) //yep, you can put directly 73, but with this, conbined with above, you can understand better
+	if len(packet.ClientPublicEphemeralKey) == 32 {
+		answer := make([]byte, 0, 1+8+64+32) //yep, you can put directly 105, but with this, conbined with above, you can understand better
 
-	signature := packet.Signature[:]
-	subscription := helpers.ConvertInt64Into8Bytes(packet.SubscriptionID)
+		signature := packet.Signature[:]
+		subscription := helpers.ConvertInt64Into8Bytes(packet.SubscriptionID)
 
-	answer = append(answer, byte(enums.REDIRECTED))
-	answer = append(answer, subscription[:]...)
-	answer = append(answer, signature...)
+		answer = append(answer, byte(enums.REDIRECTED))
+		answer = append(answer, subscription[:]...)
+		answer = append(answer, signature...)
+		answer = append(answer, packet.ClientPublicEphemeralKey...)
 
-	return answer, nil
+		return answer, nil
+	} else {
+		answer := make([]byte, 0, 1+8+64) //yep, you can put directly 73, but with this, conbined with above, you can understand better
+
+		signature := packet.Signature[:]
+		subscription := helpers.ConvertInt64Into8Bytes(packet.SubscriptionID)
+
+		answer = append(answer, byte(enums.REDIRECTED))
+		answer = append(answer, subscription[:]...)
+		answer = append(answer, signature...)
+
+		return answer, nil
+	}
 }
 
 func (packet *RedirectedPacket) GetSender() int64 {
 	return packet.OwnerID
 }
 
-func CreateRedirectedPacket(owner int64, signature [64]byte) *RedirectedPacket {
+func CreateRedirectedPacketWithoutEphemeralKey(
+	owner int64,
+	signature [64]byte,
+	subscriptionId int64) *RedirectedPacket {
 	return &RedirectedPacket{
-		OwnerID:   owner,
-		Signature: signature,
+		OwnerID:                  owner,
+		Signature:                signature,
+		SubscriptionID:           subscriptionId,
+		ClientPublicEphemeralKey: nil,
+	}
+}
+
+func CreateRedirectedPacketWithEphemeralKey(
+	owner int64,
+	signature [64]byte,
+	subscriptionId int64,
+	clientPublicEphemeralKey [32]byte) *RedirectedPacket {
+	return &RedirectedPacket{
+		OwnerID:                  owner,
+		Signature:                signature,
+		SubscriptionID:           subscriptionId,
+		ClientPublicEphemeralKey: clientPublicEphemeralKey[:],
 	}
 }
